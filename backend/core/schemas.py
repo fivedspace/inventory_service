8e386bd9ba9f24e272a67d81078cc9ec2e26b9fc
tzw160702 <tzw160702@gmail.com>
2021-05-24 09:39:37 +0800

    代码更新

--- backend/core/schemas.py
+++ backend/core/schemas.py
@@ -1,96 +1,18 @@
 #!/usr/bin/python3
-
 from typing import List, Optional
-from pydantic import Field, BaseModel
+from pydantic import BaseModel, Field
 
 
 class Type(BaseModel):
-    type: int
+    type: str = Field(min_length=1, description='类型名称')
 
 
-# 新增商品
-class Picture(BaseModel):
+class CommType(Type):
     type_id: int
-
-
-class Specification(BaseModel):
-    spec_id: int
-    spec_info_val: str
-
-
-class AddCommodityIn(BaseModel):
-    commodity_id: Optional[int] = None
-    commodity_name: str
-    quantity_in_stock: int
-    type: List[Type]
-    picture: List[Picture]
-    spec: List[Specification]
-    remark: str
-
-
-class AddCommodityOut(BaseModel):
-    commodity_id: int
-    commodity_name: str
-    quantity_in_stock: int
-    type: List[Type]
-    picture: List[Picture]
-    spec: List[Specification]
-    remark: str
 
     class Config:
         orm_mode = True
 
 
-# # 修改商品
-# class ModifyCommodityIn(BaseModel):
-#     commodity_id: int
-#     commodity_name: str
-#     quantity_in_stock: int
-#     type: List[Type]
-#     picture: List[Picture]
-#     spec: List[Specification]
-#     remark: str
 
 
-# class ModifyCommodityOut(BaseModel):
-#     commodity_id: int
-#     commodity_name: str
-#     quantity_in_stock: int
-#     type: List[Type]
-#     picture: List[Picture]
-#     spec: List[Specification]
-#     remark: str
-
-
-# 类型商品
-# class TypeID(BaseModel):
-#     commodity_type: List[type]
-#
-#
-# class AllTypeCommodityOut(BaseModel):
-#     all_type: List[ModifyCommodityOut]
-
-
-# # 查询商品
-# class GetCommodity(BaseModel):
-#     commodity_id: int
-#     commodity_name: str
-#     quantity_in_stock: int
-#     type: List[Type]
-#     picture: List[Picture]
-#     property: dict
-#     remark: str
-
-
-# 增加类型
-class AddTypeName(BaseModel):
-    type_id: Optional[int] = None
-    typename: str
-
-
-# 修改类型
-class ModifyTypeName(BaseModel):
-    type_id: int
-    typename: str
-
-# ------------------------------
--- backend/sql_app/crud.py
+++ backend/sql_app/crud.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+import sys
+
+sys.path.append('../')
+
+from core import schemas
+from sql_app import models
+from typing import List, Any
+from sqlalchemy.orm import Session
+
+
+def db_create_type(db: Session, comm_type: schemas.Type):
+    """
+    增加数据类型
+    :param db:
+    :param comm_type:
+    :return:
+    """
+    add_type = models.Type(type=comm_type.type)
+    db.add(add_type)
+    db.commit()
+    db.refresh(add_type)
+    return add_type
+
+
+def db_get_type(db: Session):
+    """
+    获取所有类型
+    :param db:
+    :return:
+    """
+    fetch_type: List[Any] = db.query(models.Type.type_id, models.Type.type).all()
+    type_list = []
+    for one in fetch_type:
+        type_list.append(one)
+    all_type = dict()
+    all_type["type_list"] = type_list
+    return all_type
+
+
+def db_modify_type(db: Session):
+    ...
